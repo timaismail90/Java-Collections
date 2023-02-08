26eb9bc6eea0c146d4fa4b7e38fb56b930e61862
fatumaIsmail <fatumaismail38@gmail.com>
2023-02-08 11:10:19 -0500

    created a multithread clas

---
+++ src/day3/Main.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class Main {
+	public static void main(String[] args) {
+
+        // Create a variable from the MyThread class here.
+		MyThread thread2 = new MyThread(20);
+
+        // Start the thread here
+		thread2.start();
+    }
+
+}
---
+++ src/day3/MyThread.java
@@ -0,0 +1,43 @@
+package day3;
+
+public class MyThread extends Thread{
+	int limitToStop;
+	// Create a constructor to initialize the above instance variable
+    MyThread(int limitToStop){
+        this.limitToStop = limitToStop;
+    
+    }
+    
+    // Override the run() method here.
+    
+	@Override
+	public void run() {
+//		System.out.println("This thread is running");
+		for(int i = 0; i <= limitToStop; i++) {
+			if(i % 2 == 1) {
+				System.out.println(i);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		}
+		
+	}
+
+	
+	
+	
+
+	
+	
+
+
+}
+
+
+    
+
